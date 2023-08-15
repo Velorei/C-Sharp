2f3a4c98700ee4a3faf3dd794b55c663e476015f
Velorei <valeriie.alexandrovna@gmail.com>
2023-08-15 17:05:59 +0300

    HelloWorld1.cs

--- HelloWorld1.cs
+++ HelloWorld1.cs
@@ -7,6 +7,8 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello WORLD!");
+	    Console.WriteLine("I'm Learning C#");
+            Console.WriteLine("It's a bit hard but awasome");
         }
     }
 }

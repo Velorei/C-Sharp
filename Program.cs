1fe49fffc89013e7db19571c512cc3ff36c06984
Velorei <valeriie.alexandrovna@gmail.com>
2023-08-16 20:14:25 +0300

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World! ");
+            Console.Write("I will print on the same line.");
+            //This is my first single comment
+            Console.WriteLine("Hello again!");
+            Console.WriteLine("So after that line I will have one more single comment"); //One more single comment
+            /* The code below will print the words Hello World to the screen, and it is amazing */
+            Console.WriteLine("Hello World!!!");
+        }
+        
+    }
+}

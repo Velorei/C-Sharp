d601ab04a28974b1ef10ee7a336f04c0a56466ad
Velorei <valeriie.alexandrovna@gmail.com>
2023-08-17 17:47:43 +0300

    Variables

---
+++ Variables.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Here are different types of variables:");
+        }
+    }
+}

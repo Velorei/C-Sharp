ecbba6df17b53b348d0143bec4cd3c08929f8342
Velorei <valeriie.alexandrovna@gmail.com>
2023-08-18 19:18:20 +0300

    Variables

--- L/Program.cs
+++ L/Program.cs
@@ -12,7 +12,21 @@ class Program
         Console.WriteLine("bool - stores values with two states: true or false");
         string name = "John";
         Console.WriteLine(name);
-
+        int myNum = 15;
+        Console.WriteLine(myNum);
+        int myNum1 = 15;
+        myNum1 = 20; // myNum is now 20
+        Console.WriteLine(myNum1);
+        int myNum3;
+        myNum3 = 15;
+        Console.WriteLine(myNum3);
+        int myNum4 = 5;
+        double myDoubleNum = 5.99D;
+        char myLetter = 'D';
+        bool myBool = true;
+        string myText = "Hello";
+        const int myNum5 = 15;
+        myNum = 20; // error
     }
 }
 

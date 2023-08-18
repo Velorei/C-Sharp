237a6bd63397b76f7de0804b368005ffde99e068
Velorei <valeriie.alexandrovna@gmail.com>
2023-08-18 19:08:58 +0300

    Variables

---
+++ L/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace L;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Variables:");
+        Console.WriteLine("int - stores integers (whole numbers), without decimals");
+        Console.WriteLine("double - stores floating point numbers, with decimals");
+        Console.WriteLine("char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes");
+        Console.WriteLine("string - stores text, such as \"Hello World\". String values are surrounded by double quotes");
+        Console.WriteLine("bool - stores values with two states: true or false");
+
+    }
+}
+

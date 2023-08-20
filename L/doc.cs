183a86c28f08f8cfaff38a2ae9be4baff370318b
Velorei <valeriie.alexandrovna@gmail.com>
2023-08-20 18:11:23 +0300

    doc.cs

--- L/doc.cs
+++ L/doc.cs
@@ -2,12 +2,21 @@ using System;
 
 namespace MyApplication
 {
-  class Program
-  {
-    static void Main(string[] args)
+
+    class Program
     {
-      string name = "John";
-      Console.WriteLine(name);  
-     }
-  }
+        static void Main(string[] args)
+        {
+            int myNum = 5;
+            double myDoubleNum = 5.99D;
+            char myLetter = 'D';
+            bool myBool = true;
+            string myText = "Hello";
+            Console.WriteLine(myNum);
+            Console.WriteLine(myDoubleNum);
+            Console.WriteLine(myLetter);
+            Console.WriteLine(myBool);
+            Console.WriteLine(myText);
+        }
+    }
 }
